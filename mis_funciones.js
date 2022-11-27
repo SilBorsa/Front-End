7b4d45bc0ac2c271cee4282204c3c939c5402f1a
Silvana <118680621+SilBorsa@users.noreply.github.com>
2022-11-26 22:17:51 -0300

    Add files via upload

---
+++ mis_funciones.js
@@ -0,0 +1,57 @@
+function cambiarFB2(){
+    document.getElementById("miFB").src="fb2.png";
+}
+
+function cambiarFB(){
+    document.getElementById("miFB").src="fb.png";
+}
+
+function cambiarPin2(){
+    document.getElementById("miPin").src="pin2.png";
+}
+
+function cambiarPin(){
+    document.getElementById("miPin").src="pin.png";
+}
+
+function cambiarLi2(){
+    document.getElementById("miLi").src="li2.png";
+}
+
+function cambiarLi(){
+    document.getElementById("miLi").src="li.png";
+}
+
+function cambiarTg2(){
+    document.getElementById("miTg").src="tg2.png";
+}
+
+function cambiarTg(){
+    document.getElementById("miTg").src="tg.png";
+}
+
+function cambiarMail2(){
+    document.getElementById("miMail").src="correo2.png";
+}
+
+function cambiarMail(){
+    document.getElementById("miMail").src="correo.png";
+}
+
+function cambiarEdit2(){
+    document.getElementById("miEdit").src="edit2.png";
+}
+
+function cambiarEdit(){
+    document.getElementById("miEdit").src="edit.png";
+}
+
+function cambiarX2(){
+    document.getElementById("miX").src="cancel2.png";
+}
+
+function cambiarX(){
+    document.getElementById("miX").src="cancel.png";
+}
+
+
