8f0ce6d0dbe8aa45c95c5334fc2de6cd2af59b5d
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-03-26 22:53:30 -0300

    Add files via upload

--- my-app/src/app/cuerpo/cab/cab.component.ts
+++ my-app/src/app/cuerpo/cab/cab.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cab',
   templateUrl: './cab.component.html',
   styleUrls: ['./cab.component.css']
 })
-export class CabComponent {
+export class CabComponent implements OnInit{
 
+  fbURL="http://www.facebook.com/Tobias.te.amo";
+  pinURL="https://ar.pinterest.com/silborsa";
+  linURL="https://www.linkedin.com/in/licspb";
+  tgmURL="https://t.me/51lv4n4_ok";
+  
+  fbSRC="../assets/face.png";
+  pinSRC="../assets/pint.png";
+  linSRC="../assets/lin.png";
+  tgmSRC="../assets/tgm.png";
+  logSRC="../assets/login.png";
+
+  ngOnInit(): void {
+    
+  }
 }
