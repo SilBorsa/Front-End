049fca5e4919340ebddca1297d1e1e9b2ae16a04
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-04-30 23:57:42 -0300

    Add files via upload

---
+++ my-app/src/app/componentes/superior/superior.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-superior',
+  templateUrl: './superior.component.html',
+  styleUrls: ['./superior.component.css']
+})
+export class SuperiorComponent implements OnInit {
+
+  ngOnInit(): void {
+    
+  }
+}
