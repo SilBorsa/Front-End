62ea7c531b005c415d7ff5fa58a81bba1c363bd9
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-04-30 23:49:04 -0300

    Add files via upload

---
+++ my-app/src/app/componentes/detalles/detalles.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalles',
+  templateUrl: './detalles.component.html',
+  styleUrls: ['./detalles.component.css']
+})
+export class DetallesComponent implements OnInit{
+
+  ngOnInit(): void {
+    
+  }
+}
