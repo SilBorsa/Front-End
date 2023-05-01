6cd641263af2d88f6d4fc65ecd2d8d065fd419b8
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-04-30 23:52:19 -0300

    Add files via upload

---
+++ my-app/src/app/componentes/detalles/laboral/laboral.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-laboral',
+  templateUrl: './laboral.component.html',
+  styleUrls: ['./laboral.component.css']
+})
+export class LaboralComponent implements OnInit {
+
+  cgnURL="https://www.argentina.gob.ar/economia/sechacienda/cgn";
+  migraURL="https://www.argentina.gob.ar/interior/migraciones";
+
+  cgnSRC="../../../../assets/logo-mecon.png";
+  migraSRC="../../../../assets/logo-migra.png";
+
+  ngOnInit(): void {
+    
+  }
+}
