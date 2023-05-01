5f8cc92f82d3ab9666b836acc7fa5e00145b15ee
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-05-01 00:02:30 -0300

    Add files via upload

---
+++ my-app/src/app/modelo/persona.model.ts
@@ -0,0 +1,12 @@
+export class persona {
+    idPersona?: number;
+    nombre: string;
+    apellido: string;
+    url_img?: string;
+
+    constructor(nombre: string, apellido: string, url_img: string) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.url_img = url_img;
+    }
+}
