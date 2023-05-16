958d3f2bdb7305b570bb47a30c19c279503219d5
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-05-15 23:34:47 -0300

    Add files via upload

---
+++ my-app/src/app/componentes/home/home-editar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-editar',
+  templateUrl: './home-editar.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeEditarComponent {
+
+}
