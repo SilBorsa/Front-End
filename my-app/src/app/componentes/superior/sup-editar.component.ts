2f920e635c96a5c457d5944c2c9aa88fcf72a421
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-05-15 23:33:29 -0300

    Add files via upload

---
+++ my-app/src/app/componentes/superior/sup-editar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sup-editar',
+  templateUrl: './sup-editar.component.html',
+  styleUrls: ['./superior.component.css']
+})
+export class SupEditarComponent implements OnInit {
+
+  ngOnInit(): void {
+    
+  }
+}
