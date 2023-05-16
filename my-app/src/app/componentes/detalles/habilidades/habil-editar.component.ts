60dee2ad75f3795ddec62a15e76bfabccbbb4c2b
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-05-15 23:32:52 -0300

    Add files via upload

---
+++ my-app/src/app/componentes/detalles/habilidades/habil-editar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habil-editar',
+  templateUrl: './habil-editar.component.html',
+  styleUrls: ['./habilidades.component.css']
+})
+export class HabilEditarComponent implements OnInit{
+
+  ngOnInit(): void {
+    
+  }
+}
