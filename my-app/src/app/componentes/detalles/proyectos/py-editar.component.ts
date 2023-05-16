3b4f8e64a1b9db49f63d6d8140da3ff3174b2c3c
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-05-15 23:35:39 -0300

    Add files via upload

---
+++ my-app/src/app/componentes/detalles/proyectos/py-editar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-py-editar',
+  templateUrl: './py-editar.component.html',
+  styleUrls: ['./proyectos.component.css']
+})
+export class PyEditarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+}
