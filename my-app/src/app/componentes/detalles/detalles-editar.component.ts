e2efeae892d4aa21a756b2a3e984fb1cc587f825
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-05-15 23:32:02 -0300

    Add files via upload

---
+++ my-app/src/app/componentes/detalles/detalles-editar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalles-editar',
+  templateUrl: './detalles-editar.component.html',
+  styleUrls: ['./detalles.component.css']
+})
+export class DetallesEditarComponent implements OnInit{
+
+  ngOnInit(): void {
+    
+  }
+}
