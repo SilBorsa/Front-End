748b2ee4bcfb9911410073879073e787597fe735
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-06-05 00:12:02 -0300

    Add files via upload
    cambios en algunos  nombres

---
+++ src/app/modelo/dura.ts
@@ -0,0 +1,21 @@
+export class Dura {
+    idPersona:number;
+    idDura?: number;
+    nombreDura: string;
+    porcentaje: number;
+    num: number;
+    giro: number;
+    colorDura: string;
+
+    constructor (idPersona: number, 
+                 nombreDura: string, 
+                 porcentaje: number, 
+                 colorDura: string) {
+        this.idPersona = idPersona;
+        this.nombreDura = nombreDura;
+        this.porcentaje = porcentaje;
+        this.num = (440 - 440 * porcentaje / 100);
+        this.giro = (3.6 * porcentaje);
+        this.colorDura = colorDura;
+    }
+}
