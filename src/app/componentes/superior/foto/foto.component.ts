b8b4b4dda409dd7c9b18142883d1753a39d79c28
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-06-12 00:06:26 -0300

    Add files via upload

--- src/app/componentes/superior/foto/foto.component.ts
+++ src/app/componentes/superior/foto/foto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/modelo/persona';
+import { MostrarFotoService } from 'src/app/service/mostrar-foto.service';
+import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
   selector: 'app-foto',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./foto.component.css']
 })
 export class FotoComponent implements OnInit {
-
   fotoURL="../assets/mini-yo.jpg";
+  editSRC = "../../../../assets/editar.png";
+  editALT="agregar, editar, eliminar";
 
-  ngOnInit(): void {
+  mostrarFoto=false;
+
+  persona: Persona = {nombrePersona:"",
+                      apellidoPersona:"",
+                      subTitulo:"",
+                      emailPersona:"",
+                      celuPersona:"",
+                      acercaPersona:"",
+                      url_imgPersona:""}; /*= new Persona("","","","","","","");*/
     
+  constructor(private personaService: PersonaService,
+              private mostrarFotoService: MostrarFotoService) { }
+  
+  ngOnInit(): void {
+    const idPersona = 1;
+    this.personaService.getPersona(idPersona)
+        .subscribe(persona => {this.persona = persona;});
   }
+
+  abrirMostrarFoto() {
+    document.body.classList.add('modal-open');
+    this.mostrarFotoService.abrirMostrarFoto();
+  }
+
 }
