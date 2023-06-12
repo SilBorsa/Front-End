23e9dfeaf1df842f587d2c6857591b9738cd59cd
Silvana <118680621+SilBorsa@users.noreply.github.com>
2023-06-12 00:01:38 -0300

    Add files via upload

--- src/app/componentes/detalles/detalles.component.ts
+++ src/app/componentes/detalles/detalles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MostrarLabService } from 'src/app/service/mostrar-lab.service';
+import { MostrarPyService } from 'src/app/service/mostrar-py.service';
 
 @Component({
   selector: 'app-detalles',
@@ -6,8 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detalles.component.css']
 })
 export class DetallesComponent implements OnInit{
+  editSRC = "../../../../assets/editar.png";
+  editALT="agregar, editar, eliminar";
 
-  ngOnInit(): void {
-    
+  mostrarLab=false;
+  mostrarPy=false;
+
+  constructor (private mostrarLabService: MostrarLabService, 
+               private mostrarPyService: MostrarPyService) {}
+
+  ngOnInit(): void { 
   }
+
+  desplazarLab() {
+    const expLab = document.getElementById('empresas');
+    if (expLab) {
+      expLab.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  abrirMostrarLab() {
+    document.body.classList.add('modal-open');
+    this.mostrarLabService.abrirMostrarLab();
+    this.desplazarLab();
+  }
+
+  desplazarPy() {
+    const proy = document.getElementById('proy');
+    if (proy) {
+      proy.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  abrirMostrarPy() {
+    document.body.classList.add('modal-open');
+    this.mostrarPyService.abrirMostrarPy();
+    this.desplazarPy();
+  }
+
 }
